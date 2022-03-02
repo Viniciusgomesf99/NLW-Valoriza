6af2ed5982108d1cea152c7cb6a7fc4acdae88a4
Vinicius Gomes Franco <72755108+Viniciusgomesf99@users.noreply.github.com>
2022-03-02 16:01:43 -0300

    configure ormconfig.json and set our migrationsDir and DB migrations table config

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,8 @@
+import "reflect-metadata";
 import express, { response } from "express";
 
+import "./database";
+
 const app = express();
-
-app.get("/test", (request, response) => {
-    return response.send("Olá Vinicius");
-});
-
-app.post("/test-post", (request,response)=> {
-    return response.send("Ola Vinicius, esse é o metodo POST");
-})
 
 app.listen(3000, () => console.log('Server is running'));
